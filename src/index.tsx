ad879e1d9d8fc97759010e6c127d77e3adaf8a28
Jeff Smykil <jeff.smykil@gmail.com>
2025-04-29 12:11:20 -0500

    Updated to react 19 and other things

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
+
 // import "./styles/styles.scss";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-
+const rootElement = document.getElementById("root");
+if (!rootElement) {
+  throw new Error("Root element not found");
+}
+const root = createRoot(rootElement);
 root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
